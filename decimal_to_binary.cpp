d22d969dcb86914f21ba654cd5193db160bcd6f3
Manav Aggarwal <121839653+gitmanav@users.noreply.github.com>
2024-08-01 07:25:20 +0530

    Add files via upload
    This is the Decimal-to-Binary conversion program.

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter a number in decimal\n";
+    cin>>n;
+    int i=0;
+    int answer=0;
+    while(n!=0)
+    {
+        int bit=n&1;
+        answer=(bit*pow(10,i))+answer;
+        n=n>>1;
+        i++;
+    }
+    cout<<answer;
+}
